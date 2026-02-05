70b98aa26cf7edace0fe8d3b410ebafca0022649
Pham Xuan Trung <trunganya@gmail.com>
2026-02-06 00:38:43 +0700

    feat: add player movement with keyboard input

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float moveSpeed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +12,12 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float xDirection = Input.GetAxisRaw("Horizontal");
+        //vector3.right = (1,0,0)
+        if((xDirection < 0 && transform.position.x <= -8.3f) || xDirection > 0 && transform.position.x >= 8.3f)
+        {
+            return;
+        }
+        transform.position += Vector3.right * moveSpeed * xDirection * Time.deltaTime;
     }
 }
